9188e7743ecb02436d9adeb66fdaf76caf367bef
A3426L <mikan7738@gmail.com>
2023-06-15 23:19:31 +0900

    [add] fake_temp(phone)

--- fake_app/ContentView.swift
+++ fake_app/ContentView.swift
@@ -9,16 +9,46 @@ import SwiftUI
 import WatchConnectivity
 
 struct ContentView: View {
+    @State var mode = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            ZStack{
+                if mode == false{
+                    Color.green
+                        .ignoresSafeArea()
+                }else if mode == true {
+                    Color.red
+                        .ignoresSafeArea()
+                }
+                VStack {
+                    //Spacer()
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                            .frame(width: 350,height: 200)
+                            .foregroundColor(Color.black)
+                        Text(mode ? "fake":"normal")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                    ZStack{
+                        Capsule()
+                            .frame(width: 200,height: 150)
+                            .foregroundColor(.yellow)
+                        Toggle("",isOn: $mode)
+                            .controlSize(.large)
+                            .labelsHidden()
+                            .padding()
+                            .tint(.red)
+                    }
+                    Spacer()
+                }
+            }.navigationTitle("Mode")
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
